2de90ab972c95a01fdb114c09a706f354b997cff
Lucas Cubile <cubilelucas@gmail.com>
2024-02-05 17:38:36 -0300

    Final calculator

--- js/index.js
+++ js/index.js
@@ -28,35 +28,13 @@ let secondNumber = ''
 let operator = ''
 let flag = false
 let displayFlag = false
+let trys = '12.5'
 
 const display = document.querySelector('.display')
 const numberButtons = Array.from(document.querySelectorAll('.number-button'))
 const clear = document.querySelector('.clear-button')
 const operatorButtons = Array.from(document.querySelectorAll('.operator-button'))
 const equal = document.querySelector('.equal-button')
-
-/*
-1. aprieto boton => trigger update display
-2. Display es 0? si, entonces display es lo que aprete
-3. flag esta en FALSO actualizo primer numero
-4. aprieto boton => trigger operador
-5. Flag esta en falso? si, entonces limpio display, actualizo operador y flag es TRUE
-6. aprieto boton => trigger update display
-7. Display es 0? si, entonces display es lo que aprete
-8. flag esta en TRUE actualizo segundo numero
-
-9. aprieto boton => trigger operador
-10. flag esta en true? si.
-11. primer y segundo numero tienen valor? si
-12. flag a falso
-13. resultado operacion
-14. muestro resultado en display y actualizo primer numero
-15. reseteo segundo numero
-16. flag a TRUE
-
-17. aprieto boton => trigger update display
-18. display es 0? No,entonces suma todo el string
- */
 
 const updateDisplay = (pressedNumber) => {
   if (display.innerText === '0' || displayFlag) {
@@ -116,11 +94,10 @@ const pressEqual = () => {
     let hasDecimals = countDecimals(result)
     if (hasDecimals > 4) result = result.toFixed(4)
     if (result > 99999999999) result = 99999999999
-    console.log(hasDecimals)
+
     display.innerText = result
     firstNumber = result
     secondNumber = ''
-    console.log(operator)
   }
 }
 

a8a4a6b7af7ab7f8b8f88547de1de012371b699e
Lucas Cubile <cubilelucas@gmail.com>
2024-02-05 16:02:18 -0300

    Working equal operations, missing chaining operators

--- js/index.js
+++ js/index.js
@@ -19,3 +19,71 @@ const operate = (number1, number2, operator) => {
       break
   }
 }
+
+let firstNumber = ''
+let secondNumber = ''
+let operator = ''
+let flag = false
+
+const display = document.querySelector('.display')
+const numberButtons = Array.from(document.querySelectorAll('.number-button'))
+const clear = document.querySelector('.clear-button')
+const operatorButtons = Array.from(document.querySelectorAll('.operator-button'))
+const equal = document.querySelector('.equal-button')
+
+const updateDisplay = (pressedNumber) => {
+  if (display.innerText === '0') {
+    display.innerText = pressedNumber
+  } else {
+    display.innerText += pressedNumber
+  }
+
+  if (!flag) {
+    firstNumber += pressedNumber
+  } else {
+    secondNumber += pressedNumber
+  }
+}
+
+const clearDisplay = () => {
+  display.innerText = '0'
+  flag = false
+}
+
+const clearValues = () => {
+  clearDisplay()
+  firstNumber = ''
+  secondNumber = ''
+  operator = ''
+}
+
+clear.addEventListener('click', clearValues)
+
+numberButtons.forEach((button) => {
+  button.addEventListener('click', () => updateDisplay(button.innerText))
+})
+
+const pressOperator = (pressedOperator) => {
+  if (!flag) {
+    clearDisplay()
+    operator = pressedOperator
+    flag = true
+  }
+}
+
+operatorButtons.forEach((button) => {
+  button.addEventListener('click', () => pressOperator(button.innerText))
+})
+
+const pressEqual = () => {
+  if (firstNumber !== '' && secondNumber !== '') {
+    flag = false
+    let result = operate(+firstNumber, +secondNumber, operator)
+    display.innerText = result
+    firstNumber = result
+    secondNumber = ''
+    console.log(firstNumber)
+  }
+}
+
+equal.addEventListener('click', pressEqual)
